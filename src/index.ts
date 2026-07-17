851e8995912723b41b7345f298c79f03282c2795
oppenheymu <oppenheymu@gmail.com>
2026-07-17 11:15:50 +0800

    refactor: 提取配置项至独立文件并更新包信息

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,9 @@
 import type { SearchObject, SearchResult } from "@koishijs/registry";
-import {
-	type Context,
-	type Dict,
-	deepEqual,
-	pick,
-	Schema,
-	sleep,
-	Time,
-} from "koishi";
+import { type Context, type Dict, deepEqual, pick, sleep } from "koishi";
+import { Config, type Receiver } from "./config";
 
+export { Config };
 export const name = "market-info";
-
-interface Receiver {
-	platform: string;
-	selfId: string;
-	channelId: string;
-	guildId?: string;
-}
-
-const Receiver: Schema<Receiver> = Schema.object({
-	platform: Schema.string().required().description("平台名称。"),
-	selfId: Schema.string().required().description("机器人 ID。"),
-	channelId: Schema.string().required().description("频道 ID。"),
-	guildId: Schema.string().description("群组 ID。"),
-});
-
-export interface Config {
-	rules: Receiver[];
-	endpoint: string;
-	interval: number;
-	showHidden: boolean;
-	showDeletion: boolean;
-	showPublisher: boolean;
-	showDescription: boolean;
-}
-
-export const Config: Schema<Config> = Schema.object({
-	rules: Schema.array(Receiver).role("table").description("推送规则列表。"),
-	endpoint: Schema.string()
-		.default("https://registry.koishi.chat/index.json")
-		.description("插件市场地址。"),
-	interval: Schema.number()
-		.default(Time.minute * 30)
-		.description("轮询间隔 (毫秒)。"),
-	showHidden: Schema.boolean()
-		.default(false)
-		.description("是否显示隐藏的插件。"),
-	showDeletion: Schema.boolean()
-		.default(false)
-		.description("是否显示删除的插件。"),
-	showPublisher: Schema.boolean()
-		.default(false)
-		.description("是否显示插件发布者。"),
-	showDescription: Schema.boolean()
-		.default(false)
-		.description("是否显示插件描述。"),
-});
 
 export function apply(ctx: Context, config: Config) {
 	ctx.i18n.define("zh", require("../locals/zh_CN"));
